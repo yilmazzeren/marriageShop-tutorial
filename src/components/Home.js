126ea7b71984626ed2729d97dc0e2b9b42296d9a
yilmazzeren <56710444+yilmazzeren@users.noreply.github.com>
2020-05-17 01:49:57 +0300

    initialized and responsive navbar

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="navbar-wrapper">
+        <div className="navbar">
+        <div className="navbar-logo">
+          <h1 style = {{cursor:"pointer"}}>EVLİLİK <span style={{color:"orange"}}>&</span> BALAYI</h1>
+          <h4 style={{color:"orange"}}>Bizi Arayın 05055050505</h4>
+        </div>
+        <div className="navbar-links">
+            
+            <ul className="navbar-items">
+              <li>ÜRÜNLER</li>
+              <li>SİPARİŞLERİM</li>
+              <li>SEPET</li>
+              <li>İLETİŞİM</li>
+            </ul>
+          </div>
+          <div className="input-wrapper">
+        <input className="search-input" placeholder="Ara" type="text" />
+        <i  className="fas fa-search"></i>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
